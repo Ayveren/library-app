c85b6d842a3b4a5447cf5dba70ddc7190dfae437
ayveren <ayverend@gmail.com>
2018-05-29 13:06:24 +0300

    deal with static files

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
@@ -12,10 +13,6 @@ app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dis
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/views/', '/index.html'));
-});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/views/', '/index.html')));
 
-app.listen(3000, function () {
-    debug(`listening on port ${chalk.green('3000')}`);
-});
+app.listen(port, () => debug(`listening on port ${chalk.green(port)}`));
